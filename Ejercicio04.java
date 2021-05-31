7c843f15e4dbd4739852e475a717263cdcbd3e25
HaroldCA <>
2021-05-31 00:50:38 +0000

    Primercommit

---
+++ Ejercicio04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio04{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int total, n;
+    System.out.print("Ingresa el total de numero: ");
+    total = in.nextInt();
+    for (int num = 1; num <= total; num++){
+      System.out.print("Ingresa un numero: ");
+      n = in.nextInt();
+      if (n>0);
+        System.out.print("El numero  "+n+" elevado al cubo es: "+n*n*n);
+      System.out.println("");
+    }
+  }
+}
